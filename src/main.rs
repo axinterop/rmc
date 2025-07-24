5d4b310f362cb3868c26c1b3b307b2d512f8a886
axinterop <axineop@gmail.com>
2025-07-24 20:34:20 +0200

    Implement Scanner trait, apply for SimpleScanner, refactor TokenType enum

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 struct Token {
-    type_: String,
+    type_: TokenType,
     value: String,
 }
 
 impl Token {
-    fn new(type_: String, value: String) -> Token {
+    fn new(type_: TokenType, value: String) -> Token {
         Token { type_, value }
     }
     fn len(&self) -> usize {
@@ -13,29 +13,58 @@ impl Token {
 
     fn end_of_file() -> Token {
         Token {
-            type_: String::from("EOF"),
+            type_: TokenType::Eof,
             value: String::from(""),
         }
     }
 
     fn dummy() -> Token {
         Token {
-            type_: String::from("Dummy"),
+            type_: TokenType::Dummy,
             value: String::from("x"),
         }
     }
 }
 
-struct Tokenizer;
+enum TokenType {
+    Eof,
+    Dummy, // temporary
+    // Simple
+    Underscore,
+    Star,
+    Newline,
+}
 
-// Tokenizer: take a markdown string and return list of Token objects
+struct Tokenizer;
+struct SimpleScanner;
+struct TextScanner;
+
+trait Scanner {
+    fn scan(input: &str) -> Option<Token>;
+}
+
+impl Scanner for SimpleScanner {
+    fn scan(input: &str) -> Option<Token> {
+        let first_char = input.chars().nth(0)?;
+        let token_type = match first_char {
+            '_' => TokenType::Underscore,
+            '*' => TokenType::Star,
+            '\n' => TokenType::Newline,
+            _ => return None,
+        };
+        Some(Token {
+            type_: token_type,
+            value: first_char.to_string(),
+        })
+    }
+}
 
 impl Tokenizer {
     pub fn tokenize(plain_markdown: String) -> Vec<Token> {
         let mut tokens = Vec::new();
         let mut draft_markdown = plain_markdown.clone();
         while !draft_markdown.is_empty() {
-            let token: Token = Self::scan_one_token(&draft_markdown);
+            let token = Self::scan_one_token(&draft_markdown);
             draft_markdown.truncate(draft_markdown.len() - token.len());
             tokens.push(token);
         }
