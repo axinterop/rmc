22615cc9bbacadb7d3560f454a2a46d16820a8a0
axinterop <axineop@gmail.com>
2025-08-02 22:30:35 +0200

    Implement EmphasizeParser, add tests; fix consumption for TextParser

--- src/main.rs
+++ src/main.rs
@@ -109,6 +109,7 @@ impl Node {
     }
 }
 
+#[derive(PartialEq)]
 enum NodeType {
     Text,
     Emphasize,
@@ -126,13 +127,39 @@ struct SentenceParser;
 
 impl Parser for TextParser {
     fn match_tokens(tokens: &mut Vec<Token>) -> Option<Node> {
-        let f = tokens.first()?;
+        if tokens.is_empty() {
+            return None;
+        }
+        let f = tokens.remove(0);
 
         if f.type_ == TokenType::Text {
             Some(Node::new(NodeType::Text, f.value.clone()))
         } else {
             None
         }
+    }
+}
+
+impl Parser for EmphasizeParser {
+    fn match_tokens(tokens: &mut Vec<Token>) -> Option<Node> {
+        if tokens.len() < 3 {
+            return None;
+        };
+        let rule_underscore = [
+            TokenType::Underscore,
+            TokenType::Text,
+            TokenType::Underscore,
+        ];
+        let rule_star = [TokenType::Star, TokenType::Text, TokenType::Star];
+
+        for i in 0..3 {
+            if tokens[i].type_ != rule_underscore[i] && tokens[i].type_ != rule_star[i] {
+                return None;
+            }
+        }
+        let value = tokens[1].value.clone();
+        tokens.drain(0..3);
+        Some(Node::new(NodeType::Emphasize, value))
     }
 }
 
@@ -144,49 +171,49 @@ fn main() {
     }
 }
 
+fn assert_vecs(vec1: &Vec<Token>, vec2: &Vec<Token>) {
+    assert!(vec1.len() == vec2.len());
+    for i in 0..vec1.len() {
+        assert!(vec1[i].type_ == vec2[i].type_);
+        assert!(vec1[i].value == vec2[i].value);
+    }
+}
+
 #[cfg(test)]
 mod tests_tokenize {
     use super::*;
 
-    fn assert_vecs(vec1: &Vec<Token>, vec2: &Vec<Token>) {
-        assert!(vec1.len() == vec2.len());
-        for i in 0..vec1.len() {
-            assert!(vec1[i].type_ == vec2[i].type_);
-            assert!(vec1[i].value == vec2[i].value);
-        }
-    }
-
     #[test]
     fn emphesize_underscore_simple() {
         let input = "_Hello_";
-        let tokens = Tokenizer::tokenize(input);
-        let result = vec![
+        let result = Tokenizer::tokenize(input);
+        let expected = vec![
             Token::new(TokenType::Underscore, "_".to_string()),
             Token::new(TokenType::Text, "Hello".to_string()),
             Token::new(TokenType::Underscore, "_".to_string()),
             Token::eof(),
         ];
-        assert_vecs(&tokens, &result);
+        assert_vecs(&result, &expected);
     }
 
     #[test]
     fn emphesize_star_simple() {
         let input = "*Hello*";
-        let tokens = Tokenizer::tokenize(input);
-        let result = vec![
+        let result = Tokenizer::tokenize(input);
+        let expected = vec![
             Token::new(TokenType::Star, "*".to_string()),
             Token::new(TokenType::Text, "Hello".to_string()),
             Token::new(TokenType::Star, "*".to_string()),
             Token::eof(),
         ];
-        assert_vecs(&tokens, &result);
+        assert_vecs(&result, &expected);
     }
 
     #[test]
     fn bold_underscore_simple() {
         let input = "__Hello__";
-        let tokens = Tokenizer::tokenize(input);
-        let result = vec![
+        let result = Tokenizer::tokenize(input);
+        let expected = vec![
             Token::new(TokenType::Underscore, "_".to_string()),
             Token::new(TokenType::Underscore, "_".to_string()),
             Token::new(TokenType::Text, "Hello".to_string()),
@@ -194,14 +221,14 @@ mod tests_tokenize {
             Token::new(TokenType::Underscore, "_".to_string()),
             Token::eof(),
         ];
-        assert_vecs(&tokens, &result);
+        assert_vecs(&result, &expected);
     }
 
     #[test]
     fn bold_star_simple() {
         let input = "**Hello**";
-        let tokens = Tokenizer::tokenize(input);
-        let result = vec![
+        let result = Tokenizer::tokenize(input);
+        let expected = vec![
             Token::new(TokenType::Star, "*".to_string()),
             Token::new(TokenType::Star, "*".to_string()),
             Token::new(TokenType::Text, "Hello".to_string()),
@@ -209,6 +236,75 @@ mod tests_tokenize {
             Token::new(TokenType::Star, "*".to_string()),
             Token::eof(),
         ];
-        assert_vecs(&tokens, &result);
+        assert_vecs(&result, &expected);
+    }
+}
+
+#[cfg(test)]
+mod tests_parsers {
+    use super::*;
+
+    #[test]
+    fn text_simple() {
+        let mut tokens = vec![
+            Token::new(TokenType::Text, "Hello".to_string()),
+            Token::eof(),
+        ];
+
+        let result = TextParser::match_tokens(&mut tokens);
+        let expected = Some(Node::new(NodeType::Text, "Hello".to_string()));
+        assert!(result.is_some());
+        assert!(expected.is_some());
+
+        let result = result.unwrap();
+        let expected = expected.unwrap();
+        assert!(result.type_ == expected.type_);
+        assert!(result.value == expected.value);
+
+        assert!(tokens.len() == 1); // Should consume matched token
+    }
+
+    #[test]
+    fn emphasize_underscore_simple() {
+        let mut tokens = vec![
+            Token::new(TokenType::Underscore, "_".to_string()),
+            Token::new(TokenType::Text, "Hello".to_string()),
+            Token::new(TokenType::Underscore, "_".to_string()),
+            Token::eof(),
+        ];
+
+        let result = EmphasizeParser::match_tokens(&mut tokens);
+        let expected = Some(Node::new(NodeType::Emphasize, "Hello".to_string()));
+        assert!(result.is_some());
+        assert!(expected.is_some());
+
+        let result = result.unwrap();
+        let expected = expected.unwrap();
+        assert!(result.type_ == expected.type_);
+        assert!(result.value == expected.value);
+
+        assert!(tokens.len() == 1); // Should consume matched tokens
+    }
+
+    #[test]
+    fn emphasize_star_simple() {
+        let mut tokens = vec![
+            Token::new(TokenType::Underscore, "*".to_string()),
+            Token::new(TokenType::Text, "Hello".to_string()),
+            Token::new(TokenType::Underscore, "*".to_string()),
+            Token::eof(),
+        ];
+
+        let result = EmphasizeParser::match_tokens(&mut tokens);
+        let expected = Some(Node::new(NodeType::Emphasize, "Hello".to_string()));
+        assert!(result.is_some());
+        assert!(expected.is_some());
+
+        let result = result.unwrap();
+        let expected = expected.unwrap();
+        assert!(result.type_ == expected.type_);
+        assert!(result.value == expected.value);
+
+        assert!(tokens.len() == 1); // Should consume matched tokens
     }
 }
